a726b282dd7307e83160e037f890adbd8bad6a26
Zak Mayfield <zakmayfield@gmail.com>
2023-10-13 09:59:09 -0400

    feat: request-account endpoint & type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { ContactFormValues } from './types';
+import { ContactFormValues, RequestAccountFormValues } from './types';
 import { validateDomain } from './middleware';
 import { transporter } from './utils';
 import axios from 'axios';
@@ -21,8 +21,6 @@ app.get('/', (req, res) => {
     message: `Hi there. 👋`,
   });
 });
-
-// TODO: consider sending a csv of the user submitted data, if possible. this could allow easy data transfer from contact form to ordering
 
 app.post('/email', async (req, res) => {
   const data: ContactFormValues = req.body;
@@ -123,6 +121,119 @@ app.post('/email', async (req, res) => {
     Email: ${email}
     Found: ${found}
     Message: ${message}
+  `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send(`Error sending email :(`);
+    } else {
+      res.status(200).send(`Email sent successfully! :)`);
+    }
+  });
+});
+
+app.post('/request-account', async (req, res) => {
+  const data: RequestAccountFormValues = req.body;
+  const isEmptyObject = (obj) => (Object.keys(obj).length === 0 ? true : false);
+
+  if (isEmptyObject(data)) {
+    return res.status(400).json({
+      error: `No data received`,
+    });
+  }
+
+  const {
+    companyName,
+    contact,
+    email,
+    phone,
+    billingAddress,
+    shippingAddress,
+    apEmail,
+    paymentMethod,
+    token,
+  } = data;
+
+  if (!token) {
+    return res.status(400).json({
+      error: 'reCAPTCHA token required',
+    });
+  }
+
+  try {
+    const response = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`
+    );
+
+    if (response.data.success) {
+      res.json({
+        message: 'Human 👨 👩',
+      });
+    } else {
+      // Check for specific error codes or error reasons
+      if (response.data['error-codes']) {
+        // Handle specific error codes
+        const errorCodes = response.data['error-codes'];
+
+        switch (true) {
+          case errorCodes.includes('missing-input-secret'):
+            return res.status(400).json({
+              message: 'The secret parameter is missing',
+            });
+
+          case errorCodes.includes('invalid-input-secret'):
+            return res.status(400).json({
+              message: 'The secret parameter is invalid or malformed',
+            });
+
+          case errorCodes.includes('missing-input-response'):
+            return res.status(400).json({
+              message: 'The response parameter is missing',
+            });
+
+          case errorCodes.includes('invalid-input-response'):
+            return res.status(400).json({
+              message: 'The response parameter is invalid or malformed',
+            });
+
+          default:
+            return res.status(400).json({
+              message: 'Failed reCAPTCHA check',
+            });
+        }
+      } else if (response.data['error']) {
+        // Handle general errors
+        return res.status(400).json({
+          message: response.data['error'],
+        });
+      } else {
+        return res.status(400).json({
+          message: 'Failed reCAPTCHA check',
+        });
+      }
+    }
+  } catch (error) {
+    // Handle network or other errors
+    return res.status(500).json({
+      message: 'An error occurred while verifying reCAPTCHA',
+    });
+  }
+
+  const mailOptions = {
+    from: process.env.USERNAME,
+    to: process.env.ADDRESS,
+    subject: `${data.companyName} | contact`,
+    text: `
+     companyName: ${companyName},
+      contact: ${contact},
+      billingAddress: ${billingAddress},
+      shippingAddress: ${shippingAddress},
+      phone: ${phone},
+      email: ${email},
+      apEmail: ${apEmail},
+      paymentMethod: ${paymentMethod},
   `,
   };
 
